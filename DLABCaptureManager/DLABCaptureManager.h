35b9870478e5b55a2542525540e9557179478bae
Takashi Mochizuki <mochi@da2.so-net.ne.jp>
2017-10-22 10:29:48 +0900

    Initial Commit

---
+++ DLABCaptureManager/DLABCaptureManager.h
@@ -0,0 +1,19 @@
+//
+//  DLABCaptureManager.h
+//  DLABCaptureManager
+//
+//  Created by Takashi Mochizuki on 2017/10/22.
+//  Copyright © 2017年 MyCometG3. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DLABCaptureManager.
+FOUNDATION_EXPORT double DLABCaptureManagerVersionNumber;
+
+//! Project version string for DLABCaptureManager.
+FOUNDATION_EXPORT const unsigned char DLABCaptureManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DLABCaptureManager/PublicHeader.h>
+
+
